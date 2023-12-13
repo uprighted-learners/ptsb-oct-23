4be22fc62b2db04365ad0c36737cbda0228cf6aa
Dan DiGangi <me@dandigangi.com>
2023-12-13 13:56:07 -0600

    Ready

---
+++ units/web-apis/dom/challenge.js
@@ -0,0 +1,46 @@
+// ! Challenge
+//
+// & Goal: This challenge is the first time you will use both HTML and Javascript to glue a page together enabling real time changes and interactions. This will require both research and implementing the DOM introduction lessons. Put Google, lesson notes, and your previous assignments to use! There will be new methods you have not seen yet in the core requirements. Extra credit will push you even further.
+//
+// ? 1) Create a new generic Github repo. This will be storage going forward for some of your work and a playground to practice git/Github. This is an unofficial official assignment.
+// "ptsb-oct-23-[firstName]-[lastName]" (all lowercase)
+//
+// ?2) Create a new branch off of main titled "[yourInitials]-hw-dom". Lowercase the name and all of your work should be saved to this branch.
+//
+// ? 3) Create a basic HTML file using the structure that makes a standard page. It should include a title. In the body add only an empty ordered list and three buttons "Create TODO", "Remove TODO", and "Change Priority".
+//
+// ? 4) Create a script tag on the page where you will write your Javascript. Look up the different places the script tag can or should be placed in an HTML file. Comment why you placed it where you did and the pro/cons based on your research.
+//
+// Use document methods! This will require researching new things:
+// ? 5a) Add an h1 with the text "TODO List" as the first element in the body
+// ? 5b) Add an h2 with the text as a date in the future as "Due: {currentDate}" (use Javascript!)
+// ? 5c) Add 4 TODOs to your ordered list and include the number after it in parentheses (ex: Walk my fish (1))
+//
+// ? 6) Use one get or querying method to return the list of all ordered list items on the page. Log it. Then remove the 3rd and re-log the list one more time.
+//
+// ? 7) Add a click interaction to your create button that adds a new item to the TODO list. It should always be added to the end of the list.
+//
+// ? 8) Add a click interaction to your remove button that removes the last item from the list.
+//
+// ? 9) Add a click interaction function to your change button. It should accept a number (index) and hex code color. When called it updates the corresponding item with the passed hex color.
+//
+// ? 10) Write an additional get or query selector that loops over each TODO in the list and sets them all to the same color.
+//
+//
+//
+//
+// & EXTRA CREDIT
+// ? 1) Change the add function to accept a number of where it should go in the list when added.
+//
+// ? 2) Change the remove or change function to pick a random item in the list.
+//
+// ? 3) Write an error handler that prevents invalid numbers from being passed to any or all of the 3 functions.
+//
+// ? 4) Style your page with inline styles or a custom CSS file linked to the page.
+//
+// ? 5) Extract your Javascript to a separate file linked to the page.
+//
+// ? 6) Create a list of predefined colors for the change priority feature. Update the change function to allow you to pass the predfined color to it and update accordingly.
+//
+// ? 7) You pick something worthy of extra credit!
+//
