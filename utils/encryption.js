15da4fbdb0c5472987c7cb0c97ecc7c12ff74205
Dan DiGangi <me@dandigangi.com>
2024-01-02 16:48:36 -0600

    server place

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-if (fs.existsSync('./.env.local')) {
-  require('dotenv').config({ path: '.env.local' });
-} else {
-  throw new Error(
-    "The secret token is only available on your instructor's machine to decrypt .ins files",
-  );
-}
+// if (fs.existsSync('../.env.local')) {
+fs.readFile('.env.local', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+//   require('dotenv').config({ path: '.env.local' });
+// } else {
+//   throw new Error(
+//     "The secret token is only available on your instructor's machine to decrypt .ins files",
+//   );
+// }
 
 // TODO
