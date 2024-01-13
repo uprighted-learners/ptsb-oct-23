38bad482cd0d5e72d94da10ab73f2412c280ed90
Dan DiGangi <me@dandigangi.com>
2024-01-12 18:29:09 -0600

    remove not needed pages, cors, data

--- projects/shopinator/server/app.js
+++ projects/shopinator/server/app.js
@@ -1,22 +1,24 @@
+'use strict'
 require('dotenv').config()
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const { paths } = require('./constants')
 const routes = require('./routes')
 const router = express.Router
 const app = express()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(paths.public))
 
-// Routes
 app.use(['/', '/store'], routes.store)
 app.use('/cart', routes.cart)
 app.use('/admin', routes.admin)
 
 app.listen(process.env.SERVER_PORT, (req, res) => {
   console.log(
-    `Server started on ${process.env.SERVER_ADDRESS}:${process.env.SERVER_PORT}`,
+    `starting ${process.env.SERVER_ADDRESS}:${process.env.SERVER_PORT}`,
   )
 })
