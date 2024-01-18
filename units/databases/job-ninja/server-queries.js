ecdc47b6d7cfd1afcf4c6dfede7a29a421f219fd
Dan DiGangi <me@dandigangi.com>
2024-01-18 16:30:15 -0600

    new file for extra intro, ins files, done

--- units/databases/job-ninja/server-queries.js
+++ units/databases/job-ninja/server-queries.js
@@ -12,18 +12,16 @@ app.use(express.static('public'))
 // Open the overall database connection
 const client = new MongoClient(`${process.env.DB_URL}/${process.env.DB_APP}`)
 
-// Open connection to specific collection
-// This is not the data but you now have methods available to query for data
-async function getCollection(collectionName) {
-  await client.connect()
-  const collection = await client
-    .db(process.env.DB_APP)
-    .collection(collectionName)
-  return collection
-}
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/companies', async (req, res) => {
+  // Open connection to specific collection
+  // This is not the data but you now have methods available to query for data
+  await client.connect()
+  const collection = await client.db(process.env.DB_APP).collection('companies')
+  res.send('done')
 })
 
 app.listen(process.env.SERVER_PORT, (req, res) => {
