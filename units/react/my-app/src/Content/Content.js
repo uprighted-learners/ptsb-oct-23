5d74bd144ad57fc4486805c77272afd163e34e6d
Dan DiGangi <me@dandigangi.com>
2024-01-29 22:19:40 -0600

    keys

--- units/react/my-app/src/Content/Content.js
+++ units/react/my-app/src/Content/Content.js
@@ -8,9 +8,9 @@ function Content(props) {
         {/* Challenge 3: If you change map() to forEach(), why doesn't it work? */}
         {/* Answer: your answer here */}
         {props.cats.map((cat, index) => {
-          // The key prop helps React better render and update during re-renders
-          // We can use the index to achieve a unique ID
-          // https://react.dev/learn/rendering-lists
+          // Why we use keys!
+          // https://react.dev/learn/rendering-lists#why-does-react-need-keys
+          // Read the whole page but this links to specifically this part ^
           return <li key={index}>{`${cat.name}-${index}`}</li>
         })}
       </ul>
