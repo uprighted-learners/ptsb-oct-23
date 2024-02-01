5568b8c7b345597fede46476b5994c109197fe88
Dan DiGangi <me@dandigangi.com>
2024-02-01 15:18:03 -0600

    .

--- units/react/my-app/src/index.js
+++ units/react/my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 
 // The starting point of all React apps
 // This gets an element from public/index.html and mounts (aka injects) the React application
@@ -22,4 +22,4 @@ root.render(<App />)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
