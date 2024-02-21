bb803dd99cb6be9b4540d9a00a2744474bbc4cce
Dan DiGangi <me@dandigangi.com>
2024-02-21 11:38:53 -0600

    added route options

--- units/react/react-router/src/index.js
+++ units/react/react-router/src/index.js
@@ -11,6 +11,7 @@ import AboutPerson from './AboutPerson'
 // Follow this tutorial! There is a lot in there but Setup, Adding a Router, Handling Not Found Errors, Nested Routes, Loading Data, and Data Writes + HTML Forms are the key one sto complete minimum.
 // Suggestion: Use this repo to continue going through the documentation/tutorials before you bring it into your projects.
 // Doc: https://reactrouter.com/en/main/start/tutorial
+// Doc: https://reactrouter.com/en/main/route/route#type-declaration (see this for what can be passed into a route or <Route />)
 const router = createBrowserRouter([
   {
     // What path to match on
